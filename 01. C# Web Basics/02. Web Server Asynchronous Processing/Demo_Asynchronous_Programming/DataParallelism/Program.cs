725922a16c5a433dd7ce66baeb13289e49a3a20f
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-23 10:31:05 +0300

    Create Program.cs

---
+++ 01. C# Web Basics/02. Web Server Asynchronous Processing/Demo_Asynchronous_Programming/DataParallelism/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataParallelism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> elements = new List<int>(){1,2,3};
+            
+
+            Parallel.For(0, elements.Count, i =>
+            {
+                Process(elements[i]);
+            });
+
+        }
+    }
+}
