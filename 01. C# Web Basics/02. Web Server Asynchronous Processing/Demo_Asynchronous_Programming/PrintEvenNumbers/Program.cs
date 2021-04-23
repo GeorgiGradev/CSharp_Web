fa7e969176017862549533a392c6737653fc6941
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-23 10:30:45 +0300

    Create Program.cs

---
+++ 01. C# Web Basics/02. Web Server Asynchronous Processing/Demo_Asynchronous_Programming/PrintEvenNumbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadEven = new Thread(PrintEvenNumbers);
+            Thread threadUneven = new Thread(PrintUnevenNumbers);
+
+            threadEven.Start();
+            threadUneven.Start();
+
+            threadEven.Join();
+            threadUneven.Join();
+        }
+
+        private static void PrintEvenNumbers()
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Thread EVEN finished work");
+        }
+
+        private static void PrintUnevenNumbers()
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Thread UNEVEN finished work");
+        }
+    }
+}
