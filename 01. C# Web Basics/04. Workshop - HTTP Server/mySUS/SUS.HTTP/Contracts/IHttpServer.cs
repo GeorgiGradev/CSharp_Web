bf54d3ccdd4d5d712c1b18692e87eaae2cbb7a0a
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-25 12:31:03 +0300

    Create IHttpServer.cs

---
+++ 01. C# Web Basics/04. Workshop - HTTP Server/mySUS/SUS.HTTP/Contracts/IHttpServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SUS.HTTP
+{
+    public interface IHttpServer
+    {
+        void AddRoute(string path, Func<HttpRequest, HttpResponse> action);
+
+        Task StartAsync(int port);
+
+        // това с двата метода, които описват HttpServer-а
+    }
+} 
+ 
+
