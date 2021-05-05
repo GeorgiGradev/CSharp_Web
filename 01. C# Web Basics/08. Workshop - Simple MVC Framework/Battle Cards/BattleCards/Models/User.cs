e2c8625ac3fb7a4d2930bb9b6bd86bff0f1ab7a6
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-05 17:29:57 +0300

    Create User.cs

---
+++ 01. C# Web Basics/08. Workshop - Simple MVC Framework/Battle Cards/BattleCards/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BattleCards.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Cards = new HashSet<UserCard>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        public virtual ICollection<UserCard> Cards { get; set; }
+    }
+}
