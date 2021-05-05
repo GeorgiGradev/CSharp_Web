be5cbd16016fe5a93c300e9607b6bc41c1bd6934
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-05 17:28:33 +0300

    Create Startup.cs

---
+++ 01. C# Web Basics/08. Workshop - Simple MVC Framework/Battle Cards/BattleCards/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace BattleCards
+{
+    using System.Collections.Generic;
+    using BattleCards.Data;
+    using Microsoft.EntityFrameworkCore;
+    using SIS.HTTP;
+    using SIS.MvcFramework;
+
+    public class Startup : IMvcApplication
+    {
+        public void Configure(IList<Route> routeTable)
+        {
+           
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            new ApplicationDbContext().Database.Migrate();
+        }
+    }
+}
