c27b55c168409e0757ced73ea551f3161a3360dc
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-21 08:36:59 +0300

    Create IProblemsService.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/Suls/Services/IProblemsService.cs
@@ -0,0 +1,17 @@
+﻿using Suls.ViewModels.Problems;
+using System.Collections.Generic;
+
+namespace Suls.Services
+{
+    public interface IProblemsService
+    {
+        void Create(string name, int points);
+
+        IEnumerable<HomePageProblemViewModel> GetAll();
+
+        string GetNameById(string Id);
+
+        ProblemViewModel GetById(string id);
+
+    }
+}
