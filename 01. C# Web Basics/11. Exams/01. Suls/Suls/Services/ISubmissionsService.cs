413e2ca3a0fed1c4f025512087291836ff3a7b9c
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-21 08:37:11 +0300

    Create ISubmissionsService.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/Suls/Services/ISubmissionsService.cs
@@ -0,0 +1,11 @@
+﻿namespace Suls.Services
+{
+    public interface ISubmissionsService
+    {
+        void Create(string problemId, string userId, string code);
+
+        void Delete(string id);
+    }
+
+
+}
