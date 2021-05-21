91bada4366c532639b2f208ae8a728337ac853e3
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-21 08:39:56 +0300

    Create Problem.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/Suls/Data/Problem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Suls.Data
+{
+    public class Problem
+    {
+        public Problem()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Submissions = new HashSet<Submission>();
+        } 
+
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        public int Points { get; set; }
+
+        public virtual ICollection<Submission> Submissions { get; set; }
+    }
+}
