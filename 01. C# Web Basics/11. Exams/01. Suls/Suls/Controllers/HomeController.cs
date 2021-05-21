a3cb1b1ced122e9374f30c2dc3571d27755cf691
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-21 08:40:17 +0300

    Create HomeController.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/Suls/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Suls.Services;
+using Suls.ViewModels.Problems;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System.Collections.Generic;
+
+namespace Suls.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProblemsService problemsService;
+
+        public HomeController(IProblemsService problemsService)
+        {
+            this.problemsService = problemsService;
+        }
+
+
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                var viewModel = this.problemsService.GetAll();
+                return this.View(viewModel, "IndexLoggedIn");
+            }
+
+            return this.View();
+        }
+    }
+}
