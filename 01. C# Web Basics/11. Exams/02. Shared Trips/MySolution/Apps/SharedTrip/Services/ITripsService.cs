12ab63cbebcdefe65a73c765cdb67296670049bc
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-25 17:37:04 +0300

    Create ITripsService.cs

---
+++ 01. C# Web Basics/11. Exams/02. Shared Trips/MySolution/Apps/SharedTrip/Services/ITripsService.cs
@@ -0,0 +1,17 @@
+﻿using SharedTrip.ViewModels.Trips;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SharedTrip.Services
+{
+    public interface ITripsService
+    {
+        void Create(AddTripInputModel trip);
+
+        IEnumerable<TripViewModel> GetAll();
+
+        TripDetailsViewModel GetDetails(string id);
+
+        public bool AddUserToTrip(string userId, string tripId);
+    }
+}
