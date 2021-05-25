85c0e8d8ad07cb4a973c4186efeab2ec8919563d
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-25 17:39:10 +0300

    Create UserTrip.cs

---
+++ 01. C# Web Basics/11. Exams/02. Shared Trips/MySolution/Apps/SharedTrip/Data/UserTrip.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedTrip.Data
+{
+   public class UserTrip
+    {
+
+        public string UserID { get; set; }
+
+        public virtual User User { get; set; }
+
+        public string TripId { get; set; }
+
+        public virtual Trip Trip { get; set; }
+    }
+}
