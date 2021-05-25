897537f143376f4de3618d2f31c7ef1aa1e1dde7
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-25 17:37:33 +0300

    Create TripViewModel.cs

---
+++ 01. C# Web Basics/11. Exams/02. Shared Trips/MySolution/Apps/SharedTrip/ViewModels/Trips/TripViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace SharedTrip.ViewModels.Trips
+{
+    public class TripViewModel
+    {
+        public string Id { get; set; }
+
+        public string StartPoint { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public string DepartureTimeAsString => this.DepartureTime.ToString("yyyy-MM-dd");
+
+        public int AvailableSeats { get; set; }
+    }
+}
