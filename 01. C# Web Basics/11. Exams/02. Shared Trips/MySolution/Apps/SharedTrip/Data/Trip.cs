de118fb6810214fe01dc321b6b555714f7a11be8
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-25 17:39:20 +0300

    Create Trip.cs

---
+++ 01. C# Web Basics/11. Exams/02. Shared Trips/MySolution/Apps/SharedTrip/Data/Trip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.Data
+{
+    public class Trip
+    {
+        public Trip()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.UserTrips = new HashSet<UserTrip>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        public string StartPoint { get; set; }
+
+        [Required]
+        public string EndPoint { get; set; }
+
+
+        public DateTime DepartureTime { get; set; }
+
+        public int Seats { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public virtual ICollection<UserTrip> UserTrips { get; set; }
+    }
+}
