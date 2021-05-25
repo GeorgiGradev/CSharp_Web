c1c4af6a16e86ba65aa01b778872dec20f23bd6d
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-25 17:37:36 +0300

    Create LoginInputModel.cs

---
+++ 01. C# Web Basics/11. Exams/02. Shared Trips/MySolution/Apps/SharedTrip/ViewModels/Users/LoginInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedTrip.ViewModels.Users
+{
+    public class LoginInputModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
+ 
