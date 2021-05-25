cfd559c260a5e5e64ec6cd02c8fc91b1c030ff7d
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-25 17:39:17 +0300

    Create User.cs

---
+++ 01. C# Web Basics/11. Exams/02. Shared Trips/MySolution/Apps/SharedTrip/Data/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.Data
+{
+    public class User
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.UserTrips = new HashSet<UserTrip>();
+        }
+
+
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public virtual ICollection<UserTrip> UserTrips { get; set; }
+
+    }
+}
