af09c0f81c16305a708e4dcc8329ffc70cfc02b9
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-25 17:39:35 +0300

    Create HomeController.cs

---
+++ 01. C# Web Basics/11. Exams/02. Shared Trips/MySolution/Apps/SharedTrip/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace SharedTrip.Controllers
+{
+    using SUS.HTTP;
+    using SUS.MvcFramework;
+
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return Redirect("/Trips/All");
+            }
+
+            return this.View(); // Върни index.cshtml от папка Views/Home
+        }
+    }
+}
