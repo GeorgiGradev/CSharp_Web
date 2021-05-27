ce041a40b93880270256580de2d3c2b4e39acb41
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-27 20:21:18 +0300

    Create HomeController.cs

---
+++ 01. C# Web Basics/11. Exams/03. Andreys/MySolution/Andreys/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Andreys.Services;
+using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace Andreys.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductsService productsService;
+
+        public HomeController(IProductsService productsService)
+        {
+            this.productsService = productsService;
+        }
+
+
+        public HttpResponse Index()
+        {
+            if (IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+
+            return this.View();
+        }
+
+        [HttpGet("/")]
+        public HttpResponse Home()
+        {
+            if (IsUserSignedIn())
+            {
+                var viewModel = this.productsService.GetAll();
+                return this.View(viewModel);
+            }
+            return this.Redirect("/Home/Index");
+        }
+    }
+}
