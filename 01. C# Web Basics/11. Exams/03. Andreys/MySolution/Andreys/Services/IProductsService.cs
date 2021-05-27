87ca88789a0d20604b0cb0b4a898bf1624d42aff
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-27 20:19:46 +0300

    Create IProductsService.cs

---
+++ 01. C# Web Basics/11. Exams/03. Andreys/MySolution/Andreys/Services/IProductsService.cs
@@ -0,0 +1,16 @@
+﻿using Andreys.ViewModels.Products;
+using System.Collections.Generic;
+
+namespace Andreys.Services
+{
+    public interface IProductsService
+    {
+        public void Add(AddProductInputModel model);
+
+        public IEnumerable<ViewAllProductsViewModel> GetAll();
+
+        public ViewProductDetailsModel GetDetails(int id);
+
+        public void DeleteProduct(int id);
+    }
+}
