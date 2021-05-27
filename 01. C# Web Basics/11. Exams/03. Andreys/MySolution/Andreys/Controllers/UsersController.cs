e04b823ea37cdaf9e93377c15d454a15fb9197b5
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-27 20:21:10 +0300

    Create UsersController.cs

---
+++ 01. C# Web Basics/11. Exams/03. Andreys/MySolution/Andreys/Controllers/UsersController.cs
@@ -0,0 +1,117 @@
+﻿using SUS.HTTP;
+using Andreys.Services;
+using SUS.MvcFramework;
+using Andreys.ViewModels.Users;
+using System.Text.RegularExpressions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Andreys.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public HttpResponse Login()
+        {
+            if (IsUserSignedIn())
+            {
+               return this.Redirect("/");
+            }
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Login(string username, string password)
+        {
+            if (IsUserSignedIn())
+            {
+               return this.Redirect("/");
+            }
+
+            var userId = this.usersService.GetUserId(username, password);
+
+            if (userId == null)
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            this.SignIn(userId);
+
+            return this.Redirect("/");
+        }
+
+        public HttpResponse Register()
+        {
+            if (IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Register(RegisterInputModel input)
+        {
+            if (IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+
+            if (string.IsNullOrEmpty(input.Username) ||
+                input.Username.Length < 4 ||
+                input.Username.Length > 10)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (!this.usersService.IsUsernameAvailable(input.Username))
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (string.IsNullOrEmpty(input.Password) ||
+                input.Password.Length < 6 ||
+                input.Password.Length > 20)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (input.Password != input.ConfirmPassword)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (string.IsNullOrEmpty(input.Email) ||
+                !new EmailAddressAttribute().IsValid(input.Email))
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (!this.usersService.IsEmailAvailable(input.Email))
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+
+            this.usersService.Create(input.Username, input.Email, input.Password);
+            return this.Redirect("/Users/Login");
+        }
+
+        public HttpResponse Logout()
+        {
+            if (IsUserSignedIn())
+            {
+                this.SignOut();
+                return this.Redirect("/");
+            }
+
+            return this.Redirect("/Users/Login");
+        }
+    }
+}
