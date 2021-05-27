5d61a4dc7cc261e5a65f0ca344b71299023c5740
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-27 20:20:05 +0300

    Create ViewAllProductsViewModel.cs

---
+++ 01. C# Web Basics/11. Exams/03. Andreys/MySolution/Andreys/ViewModels/Products/ViewAllProductsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Andreys.ViewModels.Products
+{
+    public class ViewAllProductsViewModel
+    {
+        public int Id { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
