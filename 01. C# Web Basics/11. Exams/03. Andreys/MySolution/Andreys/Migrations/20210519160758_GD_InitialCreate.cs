f51c2c26f5b73396c6a628365f91807f9d2aa3cc
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-05-27 20:19:34 +0300

    Create 20210519160758_GD_InitialCreate.cs

---
+++ 01. C# Web Basics/11. Exams/03. Andreys/MySolution/Andreys/Migrations/20210519160758_GD_InitialCreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Andreys.Migrations
+{
+    public partial class GD_InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
