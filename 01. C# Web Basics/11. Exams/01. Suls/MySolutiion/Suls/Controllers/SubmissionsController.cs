2c21433377c850bbecbd85eeefe3eb0ba430429c
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-01 13:28:37 +0300

    Create SubmissionsController.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/MySolutiion/Suls/Controllers/SubmissionsController.cs
@@ -0,0 +1,70 @@
+﻿using Suls.Services;
+using Suls.Services.Submissions;
+using Suls.ViewModels.Submissions;
+using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace Suls.Controllers
+{
+    public class SubmissionsController : Controller
+    {
+        private readonly ISubmissionsService submissionsService;
+        private readonly IProblemsService problemsService;
+
+        public SubmissionsController(ISubmissionsService submissionsService, IProblemsService problemsService)
+        {
+            this.submissionsService = submissionsService;
+            this.problemsService = problemsService;
+        }
+
+        public HttpResponse Create(string id)
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var viewModel = new SubmissionCreateViewModel
+            {
+                ProblemId = id,
+                Name = this.problemsService.GetNameById(id)
+            };
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public HttpResponse Create(SubmissionsCreateInputModel model)
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            if (string.IsNullOrEmpty(model.Code)
+                || model.Code.Length < 30
+                || model.Code.Length > 800)
+            {
+                return this.Redirect("/Submissions/Create");
+            }
+
+
+            var userId = this.GetUserId();
+            this.submissionsService.Create(model, userId);
+
+            return this.Redirect("/");
+        }
+
+
+        public HttpResponse Delete(string id)
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            this.submissionsService.Delete(id);
+
+            return this.Redirect("/");
+        }
+    }
+}
