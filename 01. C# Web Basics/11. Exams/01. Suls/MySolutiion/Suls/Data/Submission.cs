36abb7148e47149d98a8863ffa15b1de3834d885
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-01 13:28:23 +0300

    Create Submission.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/MySolutiion/Suls/Data/Submission.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Suls.Data
+{
+    public class Submission
+    {
+        public Submission()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        public string Code { get; set; }
+
+        public int AchievedResult { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string ProblemId { get; set; }
+
+        public virtual Problem Problem { get; set; }
+
+        public virtual string UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
