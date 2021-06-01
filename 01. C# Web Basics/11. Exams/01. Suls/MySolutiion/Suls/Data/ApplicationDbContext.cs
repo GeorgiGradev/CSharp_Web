56e89325edbeb5b7b3300a3e5250351618ce54c0
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-01 13:28:30 +0300

    Create ApplicationDbContext.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/MySolutiion/Suls/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Suls.Data;
+
+namespace SulsApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Server=.;Database=Suls02;Integrated Security=true;");
+            } 
+
+            base.OnConfiguring(optionsBuilder);
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+        public DbSet<Problem> Problems { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
