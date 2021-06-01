512e3c7700cd7bc21a4abc00f5ebc68aabcdbe31
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-01 13:26:28 +0300

    Create Startup.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/MySolutiion/Suls/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Suls.Services;
+using Suls.Services.Problems;
+using Suls.Services.Submissions;
+using SulsApp.Data;
+using SUS.HTTP;
+using SUS.MvcFramework;
+namespace SulsApp
+{
+    public class Startup : IMvcApplication
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.Add<IUsersService, UsersService>();
+            serviceCollection.Add<IProblemsService, ProblemsService>();
+            serviceCollection.Add<ISubmissionsService, SubmissionsService>();
+        }
+
+        public void Configure(List<Route> routeTable)
+        {
+            new ApplicationDbContext().Database.Migrate();
+        }
+    }
+}
