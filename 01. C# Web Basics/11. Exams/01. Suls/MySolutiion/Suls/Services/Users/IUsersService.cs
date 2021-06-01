a6860495068a685175e4ecda9b42605de5484a81
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-01 13:26:20 +0300

    Create IUsersService.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/MySolutiion/Suls/Services/Users/IUsersService.cs
@@ -0,0 +1,15 @@
+﻿using Suls.ViewModels.Users;
+
+namespace Suls.Services
+{
+    public interface IUsersService
+    {
+        string GetUserId(LoginInputModel model);
+
+        void CreateUser(RegisterInputModel model);
+
+        bool IsUsernameAvailable(RegisterInputModel model);
+
+        bool IsEmailAvailable(RegisterInputModel model);
+    }
+}
