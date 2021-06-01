394fc4d56dd4ac61c37a8972ab53835c70b662b8
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-01 13:26:45 +0300

    Create SubmissionViewModel.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/MySolutiion/Suls/ViewModels/Problems/SubmissionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Suls.ViewModels.Problems
+{
+    public class SubmissionViewModel
+    {
+        public string Username { get; set; }
+
+        public int AchievedResult { get; set; }
+
+        public int MaxPoints { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string SubmissionId { get; set; }
+
+        public int Percentage => (int)Math.Round(this.AchievedResult * 100M / MaxPoints);
+    }
+}
