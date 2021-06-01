4a29d674cb422d8fb264d776a6ee30472ff5d819
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-01 13:26:17 +0300

    Create SubmissionsService.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/MySolutiion/Suls/Services/Submissions/SubmissionsService.cs
@@ -0,0 +1,45 @@
+﻿using Suls.Data;
+using Suls.ViewModels.Submissions;
+using SulsApp.Data;
+using System;
+using System.Linq;
+
+namespace Suls.Services.Submissions
+{
+    class SubmissionsService : ISubmissionsService
+    {
+        private readonly ApplicationDbContext db;
+        private readonly Random random;
+
+        public SubmissionsService(ApplicationDbContext db, Random random)
+        {
+            this.db = db;
+            this.random = random;
+        }
+
+        public void Create(SubmissionsCreateInputModel model, string userId)
+        {
+
+            var problemMaxPoints = this.db.Problems.Where(x => x.Id == model.ProblemId)
+                .Select(x => x.Points).FirstOrDefault();
+            var submission = new Submission
+            {
+                Code = model.Code,
+                ProblemId = model.ProblemId,
+                AchievedResult = this.random.Next(0, problemMaxPoints + 1),
+                CreatedOn = DateTime.UtcNow,
+                UserId = userId
+            };
+
+            this.db.Submissions.Add(submission);
+            this.db.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var submission = this.db.Submissions.Find(id);
+            this.db.Submissions.Remove(submission);
+            this.db.SaveChanges();
+        }
+    }
+}
