47bac0961aa821f03c02e7d61f4c5aae34e40214
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-01 13:28:41 +0300

    Create ProblemsController.cs

---
+++ 01. C# Web Basics/11. Exams/01. Suls/MySolutiion/Suls/Controllers/ProblemsController.cs
@@ -0,0 +1,69 @@
+﻿using Suls.Services;
+using Suls.ViewModels.Problems;
+using Suls.ViewModels.Users;
+using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace Suls.Controllers
+{
+    public class ProblemsController : Controller
+    {
+        private readonly IProblemsService problemsService;
+
+        public ProblemsController(IProblemsService problemsService)
+        {
+            this.problemsService = problemsService;
+        }
+
+
+        public HttpResponse Create()
+        {
+            if (!this.IsUserSignedIn())
+            {
+               return this.Redirect("/Users/Login");
+            }
+
+            return this.View();
+        }
+
+        public HttpResponse Details(string id)
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var viewModel = this.problemsService.GetById(id);
+          
+            return this.View(viewModel);
+        }
+
+        [HttpPost] 
+        public HttpResponse Create(CreateProblemInputModel createModel)
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            string userId = GetUserId();
+
+            if (string.IsNullOrEmpty(createModel.Name)
+                || createModel.Name.Length < 5
+                || createModel.Name.Length > 20)
+            {
+                 return this.View();
+            }
+
+            if (createModel.Points < 50
+                || createModel.Points > 300)
+            {
+                return this.View();
+            }
+
+            this.problemsService.CreateProblem(createModel, userId);
+
+            return this.Redirect("/");
+        }
+    }
+}
