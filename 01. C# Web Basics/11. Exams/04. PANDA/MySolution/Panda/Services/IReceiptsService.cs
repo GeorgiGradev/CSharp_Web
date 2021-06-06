1afc255c3955aa553cb831c6f093327616b12116
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-06 17:47:04 +0300

    Create IReceiptsService.cs

---
+++ 01. C# Web Basics/11. Exams/04. PANDA/MySolution/Panda/Services/IReceiptsService.cs
@@ -0,0 +1,13 @@
+﻿using Panda.Data.Models;
+using System.Linq;
+
+namespace Panda.Services
+{
+    public interface IReceiptsService
+    {
+        void Create(decimal weight, string packageId, string recepientId);
+
+        IQueryable<Receipt> GetAll();
+
+    }
+}
