64388fe9c0dd417c6abda473d6ab1486b755f1ae
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-06 17:47:01 +0300

    Create User.cs

---
+++ 01. C# Web Basics/11. Exams/04. PANDA/MySolution/Panda/Data/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Panda.Data.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public virtual ICollection<Package> Packages { get; set; }
+
+        public virtual ICollection<Receipt> Receipts { get; set; }
+    }
+}
