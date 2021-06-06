cdbe00a18ee234b12c6655b1bd1adf5582a3b9a1
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-06 17:47:36 +0300

    Create IPackagesService.cs

---
+++ 01. C# Web Basics/11. Exams/04. PANDA/MySolution/Panda/Services/IPackagesService.cs
@@ -0,0 +1,15 @@
+﻿using Panda.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Panda.Services
+{
+    public interface IPackagesService
+    {
+        public void CreatePackage(string description, decimal weight, string shippingAddress, string recipientName);
+
+        public IQueryable<Package> GetAllByStatus(PackageStatus status);
+
+        void Deliver(string id);
+    }
+}
