af742c9c5fa6ada1787e3b9ff760c2bcdcbfa811
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-06 17:48:31 +0300

    Create Package.cs

---
+++ 01. C# Web Basics/11. Exams/04. PANDA/MySolution/Panda/Data/Models/Package.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Panda.Data.Models
+{
+    public class Package
+    {
+        public Package()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Description { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        public PackageStatus Status { get; set; }
+
+        public DateTime EstimatedDeliveryDate { get; set; }
+
+        [Required]
+        public string RecipientId { get; set; }
+
+        public virtual User Recipient { get; set; }
+    }
+}
