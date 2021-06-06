261659f7d06087bf1419862cbc3a8ba5fcec2832
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-06 17:48:59 +0300

    Create HomeController.cs

---
+++ 01. C# Web Basics/11. Exams/04. PANDA/MySolution/Panda/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using SUS.HTTP;
+using Panda.Services;
+using SUS.MvcFramework;
+using Panda.ViewModels;
+
+namespace Panda.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUsersService usersService;
+
+        public HomeController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                var userId = this.GetUserId();
+
+                var viewModel = new LoginInputModel()
+                {
+                    Username = this.usersService.GetUsernameById(userId)
+                };
+
+                return this.View(viewModel, "IndexLoggedIn");
+            }
+
+            return this.View();
+        }
+    }
+}
+
+
+
