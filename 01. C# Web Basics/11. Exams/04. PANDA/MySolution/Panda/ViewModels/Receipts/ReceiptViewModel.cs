6bca2c866920633d37d440ee21693977a116c265
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-06 17:47:29 +0300

    Create ReceiptViewModel.cs

---
+++ 01. C# Web Basics/11. Exams/04. PANDA/MySolution/Panda/ViewModels/Receipts/ReceiptViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Panda.ViewModels.Receipts
+{
+    public class ReceiptViewModel
+    {
+        public string Id { get; set; }
+
+        public decimal Fee { get; set; }
+
+        public DateTime IssuedOn { get; set; }
+
+        public string RecipientName { get; set; }
+    }
+}
