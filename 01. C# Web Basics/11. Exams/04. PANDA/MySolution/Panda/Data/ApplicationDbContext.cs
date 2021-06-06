2b12b3e07b287592175dffa3c4379a2eda0eeb9d
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-06 17:48:41 +0300

    Create ApplicationDbContext.cs

---
+++ 01. C# Web Basics/11. Exams/04. PANDA/MySolution/Panda/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Panda.Data.Models;
+
+namespace Panda.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        { }
+        public ApplicationDbContext(DbContextOptions db)
+            : base(db)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Server=.;Database=Panda;Integrated Security=true;");
+            }
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<Package> Packages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.Packages)
+                .WithOne(x => x.Recipient)
+                .HasForeignKey(x => x.RecipientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.Receipts)
+                .WithOne(x => x.Recipient)
+                .HasForeignKey(x => x.RecipientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
