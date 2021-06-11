fcb0d105ef8af024e7bcc181bc1c56592fd754c9
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-11 10:52:07 +0300

    Remove Settings Model & Functionality

--- 02. ASP.NET Core/MoiteRecepti Workshop Project/Tests/Sandbox/Program.cs
+++ 02. ASP.NET Core/MoiteRecepti Workshop Project/Tests/Sandbox/Program.cs
@@ -16,7 +16,6 @@
     using MoiteRecepti.Data.Models;
     using MoiteRecepti.Data.Repositories;
     using MoiteRecepti.Data.Seeding;
-    using MoiteRecepti.Services.Data;
     using MoiteRecepti.Services.Messaging;
 
     public static class Program
@@ -50,9 +49,7 @@
         {
             var sw = Stopwatch.StartNew();
 
-            var settingsService = serviceProvider.GetService<ISettingsService>();
-            Console.WriteLine($"Count of settings: {settingsService.GetCount()}");
-
+  
             Console.WriteLine(sw.Elapsed);
             return await Task.FromResult(0);
         }
@@ -79,7 +76,6 @@
 
             // Application services
             services.AddTransient<IEmailSender, NullMessageSender>();
-            services.AddTransient<ISettingsService, SettingsService>();
         }
     }
 }
