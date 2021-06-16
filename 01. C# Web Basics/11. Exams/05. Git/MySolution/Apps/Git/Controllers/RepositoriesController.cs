652557675a16db3e0a262bfe762a12725dbcfaf9
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-16 12:40:45 +0300

    Create RepositoriesController.cs

---
+++ 01. C# Web Basics/11. Exams/05. Git/MySolution/Apps/Git/Controllers/RepositoriesController.cs
@@ -0,0 +1,48 @@
+﻿using Git.Data;
+using Git.Services;
+using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace Git.Controllers
+{
+    public class RepositoriesController : Controller
+    {
+        private readonly IRepositoriesService repositoriesService;
+
+        public RepositoriesController(IRepositoriesService repositoriesService)
+        {
+            this.repositoriesService = repositoriesService;
+        }
+
+        public HttpResponse All()
+        {
+            var viewModel = this.repositoriesService.GetAll();
+
+            return this.View(viewModel);
+        }
+
+        public HttpResponse Create()
+        {
+            return this.View();
+
+        }
+
+        [HttpPost]
+        public HttpResponse Create(CreateRepositoryInputModel model)
+        {
+            if (string.IsNullOrEmpty(model.Name)
+               || model.Name.Length < 3
+               || model.Name.Length > 10)
+            {
+                return this.View();
+            }
+
+            var userId = GetUserId();
+
+            this.repositoriesService.CreateRepository(model.Name, model.RepositoryType, userId);
+
+            return this.Redirect("/Repositories/All");
+        }
+
+    }
+}
