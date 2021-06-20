99f1e47d36d1a2ad01b8206c42292a888c51a7f0
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:57:32 +0300

    Create ICardsService.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/Services/Cards/ICardsService.cs
@@ -0,0 +1,18 @@
+﻿using BattleCards.ViewModels.Cards;
+using System.Collections.Generic;
+
+namespace BattleCards.Services.Cards
+{
+    public interface ICardsService
+    {
+        public void CreateCard(CreateCardInputModel model, string userId);
+
+        public List<CardViewModel> GetAll();
+
+        public MyCollectionViewModel GetMyCollection(string userId);
+
+        public bool AddToCollection(int cardId, string userId);
+
+        public void RemoveCard(int cardId, string userId);
+    }
+}
