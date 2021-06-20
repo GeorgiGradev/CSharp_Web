e5a6f9711b049506e781b54dac51575de2b1ec52
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:58:33 +0300

    Create HomeController.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace BattleCards.Controllers
+{
+    using SIS.HTTP;
+    using SIS.MvcFramework;
+
+    public class HomeController : Controller
+    { 
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (IsUserLoggedIn())
+            {
+                return this.Redirect("/Cards/All");
+            }
+            
+            return this.View();
+        }
+    }
+}
