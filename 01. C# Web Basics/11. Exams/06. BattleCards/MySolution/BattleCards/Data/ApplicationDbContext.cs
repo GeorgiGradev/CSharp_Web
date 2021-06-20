2b4aa5c60e6cac18089e993e1522b5ff34e51f57
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:58:30 +0300

    Create ApplicationDbContext.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿namespace BattleCards.Data
+{
+    using BattleCards.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        { }
+        public ApplicationDbContext(DbContextOptions db)
+            : base(db)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<UserCard> UserCards { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
