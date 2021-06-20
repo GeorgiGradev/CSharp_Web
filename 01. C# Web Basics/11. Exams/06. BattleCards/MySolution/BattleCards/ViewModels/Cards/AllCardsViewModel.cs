901df3bf2ca751a8fa5781b13f4ee43894a8bff3
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:57:41 +0300

    Create AllCardsViewModel.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/ViewModels/Cards/AllCardsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BattleCards.ViewModels.Cards
+{
+   public class AllCardsViewModel
+    {
+        public List<CardViewModel> Cards { get; set; }
+    }
+}
