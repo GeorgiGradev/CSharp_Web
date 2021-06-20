efb1f42fc98ce05cedab9b322f89ced1d8718c85
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:57:17 +0300

    Create UserCard.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/Data/Models/UserCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BattleCards.Data.Models
+{
+    public class UserCard
+    {
+        public UserCard()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public int CardId { get; set; }
+
+        public virtual Card Card { get; set; }
+    }
+}
