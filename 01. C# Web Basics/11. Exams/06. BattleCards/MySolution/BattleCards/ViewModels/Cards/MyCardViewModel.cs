47ebbc8ef208746402dd6501b2a0e82504d8d26b
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:57:47 +0300

    Create MyCardViewModel.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/ViewModels/Cards/MyCardViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BattleCards.ViewModels.Cards
+{
+    public class MyCardViewModel
+    {
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public string Keyword { get; set; }
+
+        public int Attack { get; set; }
+
+        public int Health { get; set; }
+
+        public int CardId { get; set; }
+    }
+}
