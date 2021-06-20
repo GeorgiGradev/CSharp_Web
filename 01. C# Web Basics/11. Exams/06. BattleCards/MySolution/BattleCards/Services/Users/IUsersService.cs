2f4ae8c4b564bb352e04fd54eba3da240759953c
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:57:34 +0300

    Create IUsersService.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/Services/Users/IUsersService.cs
@@ -0,0 +1,15 @@
+﻿using BattleCards.ViewModels.Users;
+
+namespace BattleCards.Services.Users
+{
+    public interface IUsersService
+    {
+        string GetUserId(LoginInputModel model);
+
+        void Create(RegisterInputModel model);
+
+        bool IsUsernameAvailable(RegisterInputModel model);
+
+        bool IsEmailAvailable(RegisterInputModel model);
+    }
+}
