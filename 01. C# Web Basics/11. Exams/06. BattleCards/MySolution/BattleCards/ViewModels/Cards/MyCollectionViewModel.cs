42f78698b5cf9d2907bb1a434138a8f76cc77b8d
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:57:50 +0300

    Create MyCollectionViewModel.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/ViewModels/Cards/MyCollectionViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BattleCards.ViewModels.Cards
+{
+    public class MyCollectionViewModel
+    {
+        public List<MyCardViewModel> MyCollection { get; set; }
+    }
+}
