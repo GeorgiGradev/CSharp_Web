8c23be968bbdff001cf71474b85dcb8e1bfce4cc
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-20 16:58:23 +0300

    Create User.cs

---
+++ 01. C# Web Basics/11. Exams/06. BattleCards/MySolution/BattleCards/Data/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using SIS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BattleCards.Data.Models
+{
+    public class User : IdentityUser<string>
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Role = IdentityRole.User;
+            this.UserCards = new HashSet<UserCard>();
+        }
+
+        public virtual ICollection<UserCard> UserCards { get; set; }
+    }
+}
