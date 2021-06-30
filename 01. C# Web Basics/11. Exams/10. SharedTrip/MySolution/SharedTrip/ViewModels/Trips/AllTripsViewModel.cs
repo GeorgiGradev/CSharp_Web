81fce3c3db3c03e970a226032c1dc402cd23f4d2
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-30 14:14:35 +0300

    Create AllTripsViewModel.cs

---
+++ 01. C# Web Basics/11. Exams/10. SharedTrip/MySolution/SharedTrip/ViewModels/Trips/AllTripsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedTrip.ViewModels.Trips
+{
+    using System.Collections.Generic;
+
+    public class AllTripsViewModel
+    {
+        public ICollection<TripViewModel> Trips { get; set; }
+    }
+}
