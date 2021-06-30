05d978f0a6d1674a6b5468a338a11ca3976a92f1
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-30 14:14:26 +0300

    Create UsersService.cs

---
+++ 01. C# Web Basics/11. Exams/10. SharedTrip/MySolution/SharedTrip/Services/Users/UsersService.cs
@@ -0,0 +1,63 @@
+﻿namespace SharedTrip.Services.Users
+{
+    using System.Linq;
+    using System.Text;
+    using System.Security.Cryptography;
+
+    using SharedTrip.Data.Models;
+
+    public class UsersService : IUsersService
+    {
+        private readonly ApplicationDbContext db;
+
+        public UsersService(ApplicationDbContext db)
+
+        {
+            this.db = db;
+        }
+
+        public void Create(string username, string email, string password)
+        {
+            var user = new User
+            {
+                Username = username,
+                Email = email,
+                Password = ComputeHash(password) 
+            };
+
+            this.db.Users.Add(user); 
+            this.db.SaveChanges(); 
+        }
+
+        public string GetUserId(string username, string password)
+        {
+            var hashPassword = ComputeHash(password);
+            var user = db.Users.FirstOrDefault(x => x.Username == username && x.Password == hashPassword);
+
+            return user?.Id;
+        }
+        public bool IsEmailAvailable(string email)
+        {
+            return !db.Users.Any(x => x.Email == email);
+        }
+
+        public bool IsUsernameAvailable(string username)
+        {
+            return !db.Users.Any(x => x.Username == username);
+        }
+
+        private static string ComputeHash(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+
+            using (var hash = SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+                var hashedInputStringBuilder = new System.Text.StringBuilder(128);
+                foreach (var b in hashedInputBytes)
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                return hashedInputStringBuilder.ToString();
+            }
+        }
+    }
+}
