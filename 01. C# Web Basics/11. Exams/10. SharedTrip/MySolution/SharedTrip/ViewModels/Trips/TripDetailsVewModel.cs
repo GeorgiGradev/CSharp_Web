db76c7e33b8d088582ad62890a677f1b441dcd69
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-06-30 14:14:42 +0300

    Create TripDetailsVewModel.cs

---
+++ 01. C# Web Basics/11. Exams/10. SharedTrip/MySolution/SharedTrip/ViewModels/Trips/TripDetailsVewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SharedTrip.ViewModels.Trips
+{
+    public class TripDetailsVewModel
+    {
+        public string Id { get; set; }
+
+        public string StartPoint { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public string DepartureTime { get; set; }
+
+        public int AvailableSeats { get; set; }
+
+        public string Description { get; set; }
+    }
+}
