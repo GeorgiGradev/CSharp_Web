13563b85bc8e9a1b3694a51dd88bd0a465366109
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-07-08 17:45:52 +0300

    Create AddHeaderActionFilter.cs

---
+++ 02. ASP.NET Core/02. Application Flow, Filters & Middleware/MyFirstAspNetCoreApp/MyFirstAspNetCoreApp/Filters/AddHeaderActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MyFirstAspNetCoreApp.Filters
+{
+    public class AddHeaderActionFilter : ActionFilterAttribute, IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add("X-Info-Action-Name", context.ActionDescriptor.DisplayName);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            context.HttpContext.Response.Headers.Add("X-Info-Result-Type", context.Result.GetType().FullName);
+        }
+    }
+}
